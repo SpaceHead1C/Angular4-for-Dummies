3ee2c448b048caacc31e359b238e25b1c510b8b4
SpaceHead1C <damh17@gmail.com>
2018-12-09 23:25:20 +0300

    add interface of http responce (JSON) get users from REST-API

--- angular-mini/src/app/app.component.ts
+++ angular-mini/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UsersService } from './users.service'; // import new service
+import { RandomUsers } from './user/user.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,23 @@ import { UsersService } from './users.service'; // import new service
   styleUrls: ['./app.component.scss'],
   providers: [UsersService] // register new service
 })
+
 export class AppComponent {
   users = []; // 'this.users' later
 
   // For service injection
-  constructor(private usersService: UsersService) {} // initialize new service
+  constructor(private usersService: UsersService) { } // initialize new service
 
   // Callback for initialize
   ngOnInit() { // components life cycle stage
-    this.users = this.usersService.users
+    this.usersService.getUsers().subscribe((response: RandomUsers) => {
+      this.users = response.results.map(user => {
+        return {
+          name: `${user.name.title} ${user.name.first} ${user.name.last}`,
+          image: user.picture.large,
+          geo: `${user.location.city}; ${user.location.state}; ${user.location.street}`
+        };
+      })
+    })
   }
 }
--- angular-mini/src/app/users.service.ts
+++ angular-mini/src/app/users.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RandomUsers } from './user/user.interface';
+
+/* Can not inject any items without decorator
+ * and for this service used @Injectable
+ */
+@Injectable()
 export class UsersService {
-    users = [
-        { name: 'WFM 1' },
-        { name: 'WFM 2' },
-        { name: 'WFM 3' },
-        { name: 'WFM 4' },
-        { name: 'WFM 5' }
-    ]
+    constructor(private http: HttpClient) {} // for AJAX
+
+    getUsers() {
+        return this.http.get<RandomUsers>('https://randomuser.me/api/?inc=gender,name,picture,location&results=8&nat=gb')
+    }
 }

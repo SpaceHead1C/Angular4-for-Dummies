75430a2e9333a6fa37ef3a7e3dc4ad589fee9853
SpaceHead1C <damh17@gmail.com>
2018-12-10 01:46:04 +0300

    create 2 components via angular CLI with commands (g = generate, c = component): cd src\app ng g c home-page ng g c setup-page

--- angular-mini/src/app/app.module.ts
+++ angular-mini/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { HoverDirective } from './hover.directive';
 import { FormsModule } from '@angular/forms';
 import { SearchPipe } from './search.pipe';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SetupPageComponent } from './setup-page/setup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent, // registration new component
     HoverDirective, // registration new directive
-    SearchPipe // registration new pipe
+    SearchPipe, // registration new pipe
+    HomePageComponent,
+    SetupPageComponent
   ],
   imports: [
     BrowserModule,

1778ff47e3a414fb73e6c7ed36fe7390fc7a389b
SpaceHead1C <damh17@gmail.com>
2018-12-09 17:06:53 +0300

    add styles for new component

--- angular-mini/src/app/user/user.component.ts
+++ angular-mini/src/app/user/user.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 
-@Component({ // decorator becouse '@'
+@Component({ // decorator because '@'
     /*There are two parameters required
      * selector - aka key for calling component
      * <template> (templateUrl)
      */
     selector: 'app-user',
-    templateUrl: './user.component.html'
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss']
 })
 
 export class UserComponent {}
